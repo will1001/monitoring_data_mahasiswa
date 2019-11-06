af1e6b9d04335e5fb87adc37492a778063376606
wili1001 <wilirahmatm@gmail.com>
2019-11-06 16:52:58 +0800

    first commit

---
+++ app/src/main/kotlin/com/example/monitoring_data_mahasiswa/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.monitoring_data_mahasiswa
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
